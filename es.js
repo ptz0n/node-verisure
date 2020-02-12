f994655cead0e695d5a6c37484201bcbca0ba5c2
Erik Eng <erik@eng.se>
2020-02-12 20:19:19 +0000

    Support transactional requests

--- es.js
+++ es.js
@@ -1,8 +1,6 @@
 const axios = require('axios');
 const { parseStringPromise } = require('xml2js');
 
-// const supportedCountries = ['es', 'fr', 'pt']
-
 const client = axios.create({
   baseURL: 'https://mob2217.securitasdirect.es:12010/WebService/ws.do',
 });
@@ -10,7 +8,7 @@ const client = axios.create({
 client.interceptors.response.use(async (response) => {
   const data = await parseStringPromise(response.data);
 
-  if (data.PET.RES[0] !== 'OK') {
+  if (!data.PET || data.PET.RES[0] !== 'OK') {
     const error = new Error('Unknown response');
     error.response = {
       ...response,
@@ -21,7 +19,11 @@ client.interceptors.response.use(async (response) => {
     return Promise.reject(error);
   }
 
-  return data;
+  return data.PET;
+});
+
+const sleep = (seconds) => new Promise((resolve) => {
+  setTimeout(resolve, seconds * 1000);
 });
 
 const buildId = (user) => {
@@ -29,18 +31,79 @@ const buildId = (user) => {
   return `IPH_________________________${user}${date}`;
 };
 
-const login = (username, password, country) => client({
-  method: 'POST',
-  params: {
-    request: 'LOGIN',
-    ID: buildId(username),
-    Country: country.toUpperCase(),
-    lang: country,
-    user: username,
-    pwd: password,
-  },
-}).then(({ PET }) => PET.HASH[0]);
+// const supportedCountries = ['es', 'fr', 'pt']
 
-module.exports = {
-  login,
-};
+class SecuritasDirect {
+  constructor(username, password, country) {
+    this.params = {
+      Country: country.toUpperCase(),
+      ID: buildId(username),
+      lang: country,
+      pwd: password,
+      user: username,
+    };
+  }
+
+  login() {
+    return client({
+      method: 'POST',
+      params: {
+        ...this.params,
+        request: 'LOGIN',
+      },
+    }).then(({ HASH }) => {
+      [this.params.hash] = HASH;
+      return this.params.hash;
+    });
+  }
+
+  logout() {
+    return client({
+      method: 'GET',
+      params: {
+        ...this.params,
+        request: 'CLS',
+        numinst: '',
+      },
+    });
+  }
+
+  getInstallation(installation) {
+    return client({
+      method: 'POST',
+      params: {
+        ...this.params,
+        request: 'MYINSTALLATION',
+        numinst: installation,
+      },
+    }).then(({ INSTALLATION }) => INSTALLATION[0]);
+  }
+
+  async transaction(action, installation, panel, retries = 0) {
+    if (retries > 10) {
+      return Promise.reject(new Error('Too many retries'));
+    }
+
+    const step = retries === 0 ? 1 : 2;
+
+    const response = await client({
+      method: 'GET',
+      params: {
+        ...this.params,
+        numinst: installation,
+        panel,
+        request: `${action}${step}`,
+      },
+    }).catch(() => null);
+
+    if (response && step === 2) {
+      return response;
+    }
+
+    await sleep(2);
+
+    return this.transaction(action, installation, panel, retries + 1);
+  }
+}
+
+module.exports = SecuritasDirect;
--- es.test.js
+++ es.test.js
@@ -1,29 +1,74 @@
 const nock = require('nock');
 const tk = require('timekeeper');
-const qs = require('qs');
 
-const { login } = require('./es');
+const SecuritasDirect = require('./es');
 
 nock.disableNetConnect();
 const scope = nock('https://mob2217.securitasdirect.es:12010/WebService/').log(console.log);
 
 describe('ES', () => {
+  tk.freeze(new Date('2020-02-06T21:02:54.868Z'));
+
+  const params = {
+    Country: 'ES',
+    ID: 'IPH_________________________john20200206210254',
+    lang: 'es',
+    pwd: 'topsecret',
+    user: 'john',
+  };
+
+  const client = new SecuritasDirect('john', 'topsecret', 'es');
+
   test('login should get hash for user', async () => {
-    tk.freeze(new Date('2020-02-06T21:02:54.868Z'));
-
-    const query = qs.stringify({
+    scope.post('/ws.do').query({
+      ...params,
       request: 'LOGIN',
-      ID: 'IPH_________________________john20200206210254',
-      Country: 'ES',
-      lang: 'es',
-      user: 'john',
-      pwd: 'topsecret',
-    });
+    }).replyWithFile(200, `${__dirname}/test/responses/LOGIN.xml`);
 
-    scope.post(`/ws.do?${query}`).replyWithFile(200, `${__dirname}/test/responses/hash.xml`);
+    const hash = await client.login();
 
-    const hash = await login('john', 'topsecret', 'es');
-
+    params.hash = hash;
     expect(hash).toBe('11111111111');
+    expect(client.params.hash).toBe('11111111111');
   });
+
+  test('should get installation', async () => {
+    scope.post('/ws.do').query({
+      ...params,
+      numinst: '2423443',
+      request: 'MYINSTALLATION',
+    }).replyWithFile(200, `${__dirname}/test/responses/MYINSTALLATION.xml`);
+
+    const installation = await client.getInstallation('2423443');
+
+    expect(installation.DEVICES.length).toBe(1);
+  });
+
+  test('should get panel status', async () => {
+    const query = {
+      ...params,
+      numinst: '2423443',
+      panel: 'SDVFAST',
+    };
+
+    scope.get('/ws.do').query({
+      ...query,
+      request: 'EST1',
+    }).replyWithFile(200, `${__dirname}/test/responses/EST1.xml`);
+
+    scope.get('/ws.do').query({
+      ...query,
+      request: 'EST2',
+    }).times(3).replyWithFile(200, `${__dirname}/test/responses/EST2-WAIT.xml`);
+
+    scope.get('/ws.do').query({
+      ...query,
+      request: 'EST2',
+    }).replyWithFile(200, `${__dirname}/test/responses/EST2.xml`);
+
+    const { NUMINST, STATUS } = await client.transaction('EST', '2423443', 'SDVFAST');
+
+    expect(NUMINST[0]).toBe('2423443');
+    expect(STATUS[0]).toBe('0');
+  }, 10000);
 });
