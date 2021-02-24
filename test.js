c504056608a9f95b632476e51bdbbca3e1556063
Erik Eng <erik@eng.se>
2021-02-24 22:53:29 +0100

    Test cookie and auth host retry

--- test.js
+++ test.js
@@ -3,46 +3,59 @@ const nock = require('nock');
 const Verisure = require('./index');
 
 nock.disableNetConnect();
-const scope = nock(/https:\/\/e-api0\d.verisure.com/);
+
+const scope = nock(/https:\/\/e-api0\d.verisure.com/, {
+  reqheaders: {
+    cookie: (value) => value === 'vid=myExampleToken',
+  },
+});
 
 describe('Verisure', () => {
   const verisure = new Verisure('email', 'password');
 
   it('should get token', async () => {
-    nock(/https:\/\/automation0\d.verisure.com/)
-      .get('/auth/login').replyWithFile(200, `${__dirname}/test/responses/login.json`, {
+    const authScope = nock(/https:\/\/automation0\d.verisure.com/);
+
+    // Verify retry on different host.
+    authScope.get('/auth/login').reply(500, 'Not this one');
+
+    authScope
+      .get('/auth/login')
+      .basicAuth({ user: 'email', pass: 'password' })
+      .replyWithFile(200, `${__dirname}/test/responses/login.json`, {
         'Set-Cookie': 'vid=myExampleToken; Version=1; Path=/; Domain=verisure.com; Secure;',
       });
 
-    expect.assertions(2);
-
     const token = await verisure.getToken();
 
+    expect.assertions(3);
     expect(token).toEqual('myExampleToken');
     expect(verisure.cookie).toEqual('vid=myExampleToken');
+    expect(verisure.authHost).toEqual('automation02.verisure.com');
   });
 
-  it('should get installations', () => {
+  it('should get installations', async () => {
     scope.get(`/xbn/2/installation/search?email=${verisure.email}`)
       .replyWithFile(200, `${__dirname}/test/responses/installations.json`);
 
-    return verisure.getInstallations().then((installations) => {
-      expect.assertions(7);
-      expect(installations.length).toBe(1);
+    const installations = await verisure.getInstallations();
 
-      const [installation] = installations;
-      expect(installation.giid).toBe('123456789');
-      expect(installation.locale).toBe('sv_SE');
-      expect(installation.config.locale).toBe('sv_SE');
-      expect(typeof installation.getOverview).toBe('function');
+    expect.assertions(7);
+    expect(installations.length).toBe(1);
 
-      scope.get(`/xbn/2/installation/${installation.giid}/overview`)
-        .replyWithFile(200, `${__dirname}/test/responses/overview.json`);
-      return installation.getOverview().then((overview) => {
-        expect(typeof overview).toBe('object');
-        expect(overview.armstateCompatible).toBeTruthy();
-      });
-    });
+    const [installation] = installations;
+    expect(installation.giid).toBe('123456789');
+    expect(installation.locale).toBe('sv_SE');
+    expect(installation.config.locale).toBe('sv_SE');
+    expect(typeof installation.getOverview).toBe('function');
+
+    scope.get(`/xbn/2/installation/${installation.giid}/overview`)
+      .replyWithFile(200, `${__dirname}/test/responses/overview.json`);
+
+    const overview = await installation.getOverview();
+
+    expect(typeof overview).toBe('object');
+    expect(overview.armstateCompatible).toBeTruthy();
   });
 
   it('should retry once with different host', (done) => {
